eb590eeeb4a90906d79849058279ea7d588ba6b4
tdiccccc <the.earth3121@gmail.com>
2022-05-10 09:12:55 +0000

    [update product]

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,18 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def new
+    @product = Product.new
+    @genres = Genre.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "商品 #{@product.genre.name} を登録しました。"
+    else
+      render :new
+    end
   end
 
   def index
@@ -11,5 +22,11 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :delivery_date, :product_count)
   end
 end
